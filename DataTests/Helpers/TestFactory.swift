a716e05f2328570f07639c851a2013a2d667f631
Luciano <lpuzer@yahoo.com.br>
2023-05-31 16:57:20 -0300

    ensure AlamofireAdapter calls alamofire with correct data

--- DataTests/Helpers/TestFactory.swift
+++ DataTests/Helpers/TestFactory.swift
@@ -6,6 +6,10 @@ func makeInvalidDate() -> Data {
     return Data("".utf8)
 }
 
+func makeValidDate() -> Data {
+    return Data("{\"name\":\"Rodrigo\"}".utf8)
+}
+
 func makeURL() -> URL {
     return URL(string: "http://any-url.com")!
 }
