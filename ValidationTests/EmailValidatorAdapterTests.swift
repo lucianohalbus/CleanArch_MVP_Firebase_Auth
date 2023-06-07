b7489d37c756f772bdcba7e85321bf861c94b3d8
Luciano <lpuzer@yahoo.com.br>
2023-06-07 16:54:49 -0300

    move sut creation to a factory method

--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -16,7 +16,7 @@ public final class EmailValidatorAdapter: EmailValidator {
 final class EmailValidatorAdapterTests: XCTestCase {
 
     func test_invalid_emails() throws {
-        let sut = EmailValidatorAdapter()
+        let sut = makeSut()
         XCTAssertFalse(sut.isValid(email: "rr"))
         XCTAssertFalse(sut.isValid(email: "rr@rr"))
         XCTAssertFalse(sut.isValid(email: "rr@rr."))
@@ -24,10 +24,16 @@ final class EmailValidatorAdapterTests: XCTestCase {
     }
     
     func test_valid_emails() throws {
-        let sut = EmailValidatorAdapter()
+        let sut = makeSut()
         XCTAssertTrue(sut.isValid(email: "teste@hotmail.com"))
         XCTAssertTrue(sut.isValid(email: "teste@google.com"))
         XCTAssertTrue(sut.isValid(email: "teste@yahoo.com.br"))
         XCTAssertTrue(sut.isValid(email: "teste@icloud.com"))
+    }
+}
+
+extension EmailValidatorAdapterTests {
+    func makeSut() -> EmailValidatorAdapter {
+        return EmailValidatorAdapter()
     }
 }
