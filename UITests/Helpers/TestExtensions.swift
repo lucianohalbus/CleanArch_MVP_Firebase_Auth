6f5e5500fd3d8c8c5885f13fe43d1c6dbbbde716
Luciano <lpuzer@yahoo.com.br>
2023-06-07 13:59:26 -0300

    Move UI Extensions to a separated file

---
+++ UITests/Helpers/TestExtensions.swift
@@ -0,0 +1,19 @@
+//
+
+import Foundation
+import UIKit
+
+//Simula evento em um controle que é do tipo UIControl
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach { action in
+                (target as NSObject).perform(Selector(action))
+            }
+        }
+    }
+    
+    func simulateTap() {
+        simulate(event: .touchUpInside)
+    }
+}
--- UITests/Scenes/SignUp/SignUpViewControllerTest.swift
+++ UITests/Scenes/SignUp/SignUpViewControllerTest.swift
@@ -40,18 +40,3 @@ extension SignUpViewControllerTest {
         return sut
     }
 }
-
-//Simula evento em um controle que é do tipo UIControl
-extension UIControl {
-    func simulate(event: UIControl.Event) {
-        allTargets.forEach { target in
-            actions(forTarget: target, forControlEvent: event)?.forEach { action in
-                (target as NSObject).perform(Selector(action))
-            }
-        }
-    }
-    
-    func simulateTap() {
-        simulate(event: .touchUpInside)
-    }
-}
