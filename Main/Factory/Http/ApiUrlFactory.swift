3bca2895eb9e20d3891e1a9a34f32f0c0d21c2ff
Luciano <lpuzer@yahoo.com.br>
2023-06-26 16:46:38 -0300

    signUp working

--- Main/Factory/Http/ApiUrlFactory.swift
+++ Main/Factory/Http/ApiUrlFactory.swift
@@ -2,13 +2,6 @@ import Foundation
 
 func makeApiUrl(path: String) -> URL {
     let environment: EnvironmentVar = .debug
-    let key = environment.firebaseApiKey
-    print("key key \(key)")
-    let url = URL(string: "\(environment.path.firebaseApiBaseUrl)\(path)\(key)")!
-    
-    
-print("zico path \(url)")
-
-
+    let url = URL(string: "\(environment.path.firebaseApiBaseUrl)\(path)")!
     return url
 }
